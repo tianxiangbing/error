5d18dbd98aa47a96c1d23b159104bd443f73a0db
田想兵 <tianxb@shinemo.com>
2016-05-18 18:49:48 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+'use strict';
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var fileinclude = require('gulp-file-include');
+var rename = require("gulp-rename");
+
+gulp.task('sass', function () {
+  gulp.src('./sass/ub.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(rename('css.css'))
+    .pipe(gulp.dest('./ub/css'));
+ gulp.src('./sass/masheng.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(rename('css.css'))
+    .pipe(gulp.dest('./masheng/css'));
+});
+/*gulp.task('rename',function(){
+    gulp.src('./ub/css/*.css')
+    .pipe(rename('./ub/css/css.css'))
+    .pipe(gulp.dest('./'));
+    gulp.src('./masheng/css/*.css')
+    .pipe(rename('./masheng/css/css.css'))
+    .pipe(gulp.dest('./'));
+});*/
+
+gulp.task('watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./temp/**/*.html', ['fileinclude']);
+});
+
+gulp.task('fileinclude', function() {
+  gulp.src(['./temp/**/*.html'])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest('./ub'))
+    .pipe(gulp.dest('./masheng'));
+});
+gulp.task('default',['sass','fileinclude'])
